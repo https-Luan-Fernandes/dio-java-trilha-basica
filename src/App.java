62e7199e0cacc2850b88671e522390f6ef1eaaad
Luan Fernandes <jose.luan@academico.ifpb.edu.br>
2024-06-30 11:39:46 -0300

    feat: adicionando fluxo do programa para interacao com o cliente

--- src/App.java
+++ src/App.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+import conta.Atendente;
+import conta.ContaTerminal;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        
+    public static void main(String[] args) {
+        try (Scanner input = new Scanner(System.in)) {
+            Atendente atendente = new Atendente(null);
+            ContaTerminal conta = new ContaTerminal();
+
+            atendente.perguntarNome();
+            conta.setNomeCliente(input.nextLine());
+
+            atendente.perguntarAgencia();
+            conta.setAgencia(input.nextLine());
+
+            atendente.perguntarNumeroConta();
+            conta.setNumero(input.nextInt());
+
+            atendente.perguntarSaldoDeposito();
+            conta.setSaldo(input.nextFloat());
+
+            atendente.finalizarAtendimento(conta);
+        }
     }
 }
--- src/conta/Atendente.java
+++ src/conta/Atendente.java
@@ -4,31 +4,39 @@ public class Atendente {
 
     private String nome;
 
-    public Atendente(String nome){
+    public Atendente(String nome) {
         this.nome = nome;
     }
 
-    public String getNome(){
+    public String getNome() {
         return this.nome;
     }
 
-    public void setNome(String nome){
+    public void setNome(String nome) {
         this.nome = nome;
     }
 
-    public void perguntarAgencia(){
-        System.out.println("Por favor, digite o número da Agência !");
+    public void perguntarAgencia() {
+        System.out.println("Por favor, digite o número da Agência!");
     }
 
-    public void perguntarNumeroConta(){
-        System.out.println("Por favor, digite o número da Conta !");
+    public void perguntarNumeroConta() {
+        System.out.println("Por favor, digite o número da Conta!");
     }
 
-    public void perguntarNome(){
-        System.out.println("Por favor, digite o seu nome !");
+    public void perguntarNome() {
+        System.out.println("Por favor, digite o seu nome!");
     }
 
-    public void perguntarSaldoDeposito(){
-        System.out.println("Por favor, digite o valor do deposito !");
+    public void perguntarSaldoDeposito() {
+        System.out.println("Por favor, digite o valor do depósito!");
+    }
+
+    public void finalizarAtendimento(ContaTerminal conta) {
+        String mensagem = String.format(
+            "Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque.",
+            conta.getNomeCliente(), conta.getAgencia(), conta.getNumero(), conta.getSaldo()
+        );
+        System.out.println(mensagem);
     }
 }
